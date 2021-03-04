f00e5d4aa1100f835b045d4145b6141467afc498
Diego Martins <diego-victor25@hotmail.com>
2021-03-04 08:21:05 -0300

    terceiro commit

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require ('express');
-
 const app = express();
-
 
 
 // middlewares
@@ -9,12 +7,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 // routes
-app.use(require('../routes/rota'));
-
+app.use(require('./routes/rota'));
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log(' Servidor rodando na port %s', port);
 });
-//app.listen(8080);
-//console.log('Rodando teste!!!');
